b5f98d1ad03dc479ed9a09c7dbbd64e45e1b123f
zhuqingshuang <zhuqingshuang@people-ai.cn>
2025-10-15 17:58:11 +0800

    Modify toolchain

--- CMake/aarch64-toolchain.cmake
+++ CMake/aarch64-toolchain.cmake
@@ -4,15 +4,15 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # specified cross-compiler prefix
-set(tools /opt/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin)
-set(CMAKE_C_COMPILER   ${tools}/aarch64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/aarch64-linux-gnu-g++)
+set(tools /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin)
+set(CMAKE_C_COMPILER   ${tools}/aarch64-none-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/aarch64-none-linux-gnu-c++)
 
 # specified root path for searching libraries and headers (adjust according to your installation path)
-set(CMAKE_FIND_ROOT_PATH /opt/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/aarch64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu)
 
 # tell CMake to search for cross dependencies in these paths, not the host environment
-set(CMAKE_SYSROOT /opt/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/aarch64-linux-gnu/libc)
+set(CMAKE_SYSROOT /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
