fd31d8f6948339889500b53070584a928f582cd2
zhuqingshuang <zhuqingshuang@people-ai.cn>
2025-10-17 15:12:36 +0800

    Set the 'CMAKE_FIND_ROOT_PATH_MODE_PACKAGE' variable

--- CMake/aarch64-toolchain.cmake
+++ CMake/aarch64-toolchain.cmake
@@ -8,11 +8,13 @@ set(tools /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin)
 set(CMAKE_C_COMPILER   ${tools}/aarch64-none-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER ${tools}/aarch64-none-linux-gnu-c++)
 
+# specified target system root path
+set(CMAKE_SYSROOT /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc)
 # specified root path for searching libraries and headers (adjust according to your installation path)
 set(CMAKE_FIND_ROOT_PATH /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu)
 
 # tell CMake to search for cross dependencies in these paths, not the host environment
-set(CMAKE_SYSROOT /opt/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
